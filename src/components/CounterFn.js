a60067464a69700b76f77f5bb7ce40b58d0ad60c
Dmitro <dimavoluychik@gmail.com>
2022-06-29 15:46:58 +0300

    add styles

--- src/components/CounterFn.js
+++ src/components/CounterFn.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
+import '../styles/App.css'
+
 
 function CounterFn(){
 
-    let [firstName, setFirstName] = useState(null);
-    let [secondName, setSecondName] = useState(null);
-    let [email, setEmail] = useState(null);
+    let [firstName, setFirstName] = useState('');
+    let [secondName, setSecondName] = useState('');
+    let [email, setEmail] = useState('');
     let [arrayUsers, setArrayUsers] = useState([]);
    
     let addUser = () =>{
         
-        if(firstName != null && secondName != null && email != null){
+        if(firstName != '' && secondName != '' && email != ''){
             let fullName = firstName + ' ' + secondName;
             let setId = Math.floor(Math.random() * (100000 - 1)) + 1;
             setArrayUsers([...arrayUsers, {name: fullName, email, id: setId}]);
@@ -24,37 +26,41 @@ function CounterFn(){
     }
 
     let sortArrayUsers = arrayUsers.map(pr =>(
-        <div key={pr.id}>
-            <label>{ pr.name }</label>
-            <br/>
-            <label>{ pr.email }</label>
-            <br/>
-            <button onClick={() => removeUser(pr.id)} type="button">Remove user</button>
+        <div className="wrapper__users" key={pr.id}>
+            <div>
+                <label>{ pr.name }</label>
+                <label style={{display: 'block', paddingTop: '10px'}} >{ pr.email }</label>
+            </div>
+           
+            <div>
+                <button onClick={() => removeUser(pr.id)} type="button">Remove user</button>
+            </div>
         </div>
     ));
 
-    return <div>
+    return <div className='wrapper'>
         <div>
             <h1>List users</h1>
-        </div>
-        <div>
-            <label>First name</label>
-            <br/>
-            <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)}></input>
-            <br/>
-            <label>Second name</label>
-            <br/>
-            <input type="text" value={secondName} onChange={(e) => setSecondName(e.target.value)}></input>
-            <br/>
-            <label>Email</label>
-            <br/>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
 
-            <button type="button" onClick={addUser}>Add</button>
-        </div>
-        <div>
+            <div className="wrapper__form">
+                <label>First name</label>
+                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)}></input>
+
+                <label>Second name</label>
+                <input type="text" value={secondName} onChange={(e) => setSecondName(e.target.value)}></input>
+
+                <label>Email</label>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+
+                <button type="button" onClick={addUser}>Add</button>
+            </div>
+            
+            
             { sortArrayUsers }
+            
+            
         </div>
+        
     </div>
 }
 
